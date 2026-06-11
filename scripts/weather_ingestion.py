cf2b38e5879d8a6639d889a8679d327c57136211
PRAKALP-PANDE <prakalpsp16@gmail.com>
2026-06-12 01:55:47 +0530

    ENH: Fetching Data for Multiple Cities

--- scripts/weather_ingestion.py
+++ scripts/weather_ingestion.py
@@ -8,24 +8,30 @@ load_dotenv()
 
 API_KEY = os.getenv("WEATHER_API_KEY")
 
-CITY = "Pune"
+CITIES = [
+    "Pune",
+    "Mumbai",
+    "Delhi",
+    "Bangalore",
+    "Hyderabad"
+]
 
-URL = (
-    f"https://api.openweathermap.org/data/2.5/weather"
-    f"?q={CITY}&appid={API_KEY}&units=metric"
-)
+for city in CITIES:
+    
+    url = (
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?q={city}&appid={API_KEY}&units=metric"
+    )
 
-response = requests.get(URL)
+    response = requests.get(url)
 
-print("Status Code:", response.status_code)
+    weather_data = response.json()
 
-weather_data = response.json()
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = f"data/raw/weather_{city}_{timestamp}.json"
 
-file_path = f"data/raw/weather_{CITY}_{timestamp}.json"
+    with open(file_path, "w") as file:
+        json.dump(weather_data, file, indent=4)
 
-with open(file_path, "w") as file:
-    json.dump(weather_data, file, indent=4)
-
-print(f"Data saved to {file_path}")
+    print(f"Saved: {city}")
